f976de3f4456112e05d484d5aa8bd984035f7f32
R.I.Pienaar <rip@devco.net>
2018-03-30 17:15:26 +0200

    add data for operatingsystem packages

--- site/profiles/manifests/common.pp
+++ site/profiles/manifests/common.pp
@@ -1,3 +1,9 @@
 class profiles::common {
+  lookup("extra_packages", Array[String], "unique").each |$package| {
+    package{$package:
+      ensure => present
+    }
+  }
+
   include mcollective
 }
