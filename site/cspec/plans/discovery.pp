2526082edff427dd2f8fe80c48913b878a3b8db0
R.I.Pienaar <rip@devco.net>
2018-04-19 09:51:10 +0200

    (#1) make sure regex facts filters are tested

--- site/cspec/plans/discovery.pp
+++ site/cspec/plans/discovery.pp
@@ -3,15 +3,17 @@ plan cspec::discovery (
   Boolean $fail_fast = false,
   String $report,
 ) {
-  $args = {
-    "all_nodes"         => choria::data("discovery.all_nodes", $ds),
-    "mcollective_nodes" => choria::data("discovery.mcollective_nodes", $ds),
-    "filtered_nodes"    => choria::data("discovery.filtered_nodes", $ds),
-    "fact_filter"       => choria::data("discovery.fact_filter", $ds),
-    "fail_fast"         => $fail_fast,
-    "report"            => $report,
-  }
+  choria::data("discovery.fact_filter", $d).each |$ff| {
+    $args = {
+      "all_nodes"         => choria::data("discovery.all_nodes", $ds),
+      "mcollective_nodes" => choria::data("discovery.mcollective_nodes", $ds),
+      "filtered_nodes"    => choria::data("discovery.filtered_nodes", $ds),
+      "fact_filter"       => $ff,
+      "fail_fast"         => $fail_fast,
+      "report"            => $report,
+    }
 
-  choria::run_playbook("cspec::discovery::mc_method", $args)
-  choria::run_playbook("cspec::discovery::choria_method", $args)
+    choria::run_playbook("cspec::discovery::mc_method", $args)
+    choria::run_playbook("cspec::discovery::choria_method", $args)
+  }
 }
